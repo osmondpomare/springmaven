1979e24d06b82847e3daf06b8bd84fa32ca1cad4
osmondpomare <osmond92@gmail.com>
2019-06-26 00:52:55 -0500

    basic configuration

---
+++ src/main/java/com/osmond/springmaven/config/AppConfig.java
@@ -0,0 +1,24 @@
+package com.osmond.springmaven.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="com.osmond.springmaven")
+public class AppConfig {
+	
+	//view resolver
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+		internalResourceViewResolver.setPrefix("/WEB-INF/views/");
+		internalResourceViewResolver.setSuffix(".jsp");
+		return internalResourceViewResolver;
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Spring Maven</title>
+</head>
+<body>
+	<h2>Spring Maven Index</h2>
+	<hr>
+	
+	Spring Maven Template
+	
+</body>
+</html>
